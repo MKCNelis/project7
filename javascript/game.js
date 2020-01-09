e4572734c32f722a653a3bd4fff319d7d5c3915d
mkcnelis <maikel.nelis@gmail.com>
2020-01-09 12:36:29 +0100

    making a beginning  for the game

--- javascript/game.js
+++ javascript/game.js
@@ -1,6 +1,17 @@
 
-const textElement = document.getElementById('text')
-const optionButtonsElement = document.getElementById('option-buttons')
+const textElement = document.getElementById('text');
+const optionButtonsElement = document.getElementById('option-buttons');
+var imageElement= document.getElementById('visuals');
+var usedImage = "<img src='../visuals/Sprite_line_Bunny_basic.png'>";
+var image1 = "<img src='../visuals/hairpin musashi.png'>";
+
+function createImageElement() {
+  imageElement.innerHTML = usedImage;
+}
+
+function nextImage(x) {
+  usedImage = image1;
+}
 
 let state = {}
 
@@ -43,45 +54,29 @@ function selectOption(option) {
 const textNodes = [
   {
     id: 1,
-    text: 'You wake up in a strange place and you see a jar of blue goo near you.',
-    outputTekst.innerHTML = "<img src="visuals\Sprite_line_bunny_basic.png">"
+    text: 'Bunny wakes up form slumber.',
+    
     options: [
       {
-        text: 'Take the goo',
-        setState: { blueGoo: true },
-        nextText: 2
-      },
-      {
-        text: 'Leave the goo',
+        text: 'goes outside hole',
         nextText: 2
       }
     ]
   },
   {
     id: 2,
-    text: 'You venture forth in search of answers to where you are when you come across a merchant.',
+    text: 'sees its childern dead on the Ground.',
     options: [
+      
       {
-        text: 'Trade the goo for a sword',
-        requiredState: (currentState) => currentState.blueGoo,
-        setState: { blueGoo: false, sword: true },
-        nextText: 3
-      },
-      {
-        text: 'Trade the goo for a shield',
-        requiredState: (currentState) => currentState.blueGoo,
-        setState: { blueGoo: false, shield: true },
-        nextText: 3
-      },
-      {
-        text: 'Ignore the merchant',
+        text: 'curses to the heavens',
         nextText: 3
       }
     ]
   },
   {
     id: 3,
-    text: 'After leaving the merchant you start to feel tired and stumble upon a small town next to a dangerous looking castle.',
+    text: 'bunny shouses to take revenge.',
     options: [
       {
         text: 'Explore the castle',
